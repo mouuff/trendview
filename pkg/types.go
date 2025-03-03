48c5ba25af897227987c679b001c97f1752d345f
Arnaud Aliès <arnaudalies.py@gmail.com>
2025-03-03 12:12:50 +0100

    Update types.go

--- pkg/types.go
+++ pkg/types.go
@@ -1,1 +1,14 @@
 package pkg
+
+type ProviderConfiguration struct {
+	Model  string
+	Prompt string
+}
+
+type ConfidenceResult struct {
+	Confidence int `json:"confidence"`
+}
+
+type ConfidenceProvider interface {
+	Predict(config ProviderConfiguration, news string) (ConfidenceResult, error)
+}
