a4da30d42b3762157d92fc746208d8273cb584dc
Arnaud Aliès <arnaudalies.py@gmail.com>
2025-03-03 12:04:57 +0100

    Update main.go

--- cmd/trendview/main.go
+++ cmd/trendview/main.go
@@ -20,7 +20,7 @@ type Property struct {
 }
 
 type DataOut struct {
-	Temperature string `json:"temperature"`
+	Confidence int `json:"confidence"`
 }
 
 func main() {
@@ -28,21 +28,13 @@ func main() {
 	p := Schema{
 		Type: "object",
 		Properties: map[string]Property{
-			"capital": {
-				Type: "string",
-			},
 			"confidence": {
 				Type: "integer",
 			},
 		},
-		Required: []string{"capital", "confidence"},
+		Required: []string{"confidence"},
 	}
 
-	//p := DataOut{
-	//	Temperature: "integer",
-	//}
-
-	// Step 2: Marshal the struct to JSON bytes
 	data, err := json.Marshal(p)
 	if err != nil {
 		fmt.Println("Error marshaling:", err)
@@ -56,9 +48,11 @@ func main() {
 
 	fmt.Println(string(data))
 
+	news := "President Trump will host the first White House Crypto Summit on Friday March 7. Attendees will include prominent founders, CEOs, and investors from the crypto industry. Look forward to seeing everyone there!"
+
 	req := &api.GenerateRequest{
 		Model:  "mistral",
-		Prompt: "What is the capital of Norway?",
+		Prompt: "Based solely on the news provided below, evaluate your confidence in investing in Bitcoin on a scale from 0 to 100, where 0 represents no confidence at all (strong belief that investing would be unwise), 50 represents a neutral or mitigated stance (uncertain or balanced risk), and 100 represents very high confidence (strong belief that investing is a good opportunity). Consider factors such as market trends, regulatory developments, economic indicators, or other relevant details mentioned in the news. Provide a specific confidence score. News: " + news,
 		Format: data,
 
 		// set streaming to false
