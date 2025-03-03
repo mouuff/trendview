b9aad80a3e1e16bed77c0c3b40ec5cf0d5cae089
Arnaud Aliès <arnaudalies.py@gmail.com>
2025-03-03 14:50:32 +0100

    Update main.go

--- cmd/trendview/main.go
+++ cmd/trendview/main.go
@@ -16,25 +16,22 @@ func main() {
 		return
 	}
 
-	prompt := "Based solely on the news below, rate your confidence in investing in Bitcoin from 0 (no confidence, unwise) to 50 (neutral) to 100 (high confidence, good opportunity), considering market trends, regulations, or economic factors. If the news isn’t relevant to Bitcoin or crypto, state so, score it 50, and explain. Provide a specific score and reasoning. News: "
+	prompt := "Based solely on the news below, rate your confidence in investing in Bitcoin from 0 (no confidence, unwise) to 50 (neutral) to 100 (high confidence, good opportunity), considering market trends, regulations, or economic factors. If the news isn't relevant, score it 50. News: "
 
-	newsA := "BITCOIN PI CYCLE TOP PREDICTION SHOWDOWN! 🚨 @BitcoinProMag predicts $236,598 on Sep 28, 2025! @BitboBTC x @PositiveCrypto calls for $258,263 on Dec 2, 2025! WHO WILL BE RIGHT? Will history repeat with the Pi Cycle’s perfect track record?"
-	newsB := "President Trump will host the first White House Crypto Summit on Friday March 7. Attendees will include prominent founders, CEOs, and investors from the crypto industry. Look forward to seeing everyone there!"
-
-	result, err := pro.PredictConfidence(ctx, prompt+newsA)
-
-	if err != nil {
-		fmt.Println("Error predicting confidence:", err)
-		return
+	newsList := []string{
+		"Bitcoin will be banned in the US in 2026",
+		"HODL BITCOIN",
+		"BITCOIN PI CYCLE TOP PREDICTION SHOWDOWN! 🚨 @BitcoinProMag predicts $236,598 on Sep 28, 2025! @BitboBTC x @PositiveCrypto calls for $258,263 on Dec 2, 2025! WHO WILL BE RIGHT? Will history repeat with the Pi Cycle’s perfect track record?",
+		"President Trump will host the first White House Crypto Summit on Friday March 7. Attendees will include prominent founders, CEOs, and investors from the crypto industry. Look forward to seeing everyone there!",
 	}
-	fmt.Println(result.Confidence)
 
-	result, err = pro.PredictConfidence(ctx, prompt+newsB)
-
-	if err != nil {
-		fmt.Println("Error predicting confidence:", err)
-		return
+	for _, news := range newsList {
+		result, err := pro.PredictConfidence(ctx, prompt+news)
+		if err != nil {
+			fmt.Println("Error predicting confidence:", err)
+			return
+		}
+		fmt.Println(result.Confidence)
 	}
-	fmt.Println(result.Confidence)
 
 }
