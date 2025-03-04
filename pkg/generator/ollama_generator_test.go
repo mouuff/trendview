23c0ced944cf417a559dbb6eae5dc01198c3ff3c
Arnaud Aliès <arnaudalies.py@gmail.com>
2025-03-04 09:37:41 +0100

    Update ollama_generator_test.go

--- pkg/generator/ollama_generator_test.go
+++ pkg/generator/ollama_generator_test.go
@@ -18,6 +18,9 @@ func TestOllamaGeneratorGenerateConfidence(t *testing.T) {
 	baseprompt := "Based solely on the news below, rate your confidence in investing in Bitcoin from 0 (no confidence, unwise) to 50 (neutral) to 100 (high confidence, good opportunity), considering market trends, regulations, or economic factors. If the news isn't relevant, score it 50. News: "
 
 	testGenerateConfidence(t, ctx, gen, baseprompt+"Bitcoin will be banned in the US in 2026", 0, 20)
+	testGenerateConfidence(t, ctx, gen, baseprompt+"Bitcoin Falls 10%, Solana Crashes 20%: Why Is Crypto Market Down Today? - News18", 0, 40)
+	testGenerateConfidence(t, ctx, gen, baseprompt+"El Salvador Increases Bitcoin Holdings, How Much BTC Does It Own Now? - Watcher Guru", 60, 90)
+	testGenerateConfidence(t, ctx, gen, baseprompt+"El Salvador buys Bitcoin dip, adding 5 BTC amid price plunge to $83,000 - Crypto Briefing", 60, 90)
 	testGenerateConfidence(t, ctx, gen, baseprompt+"President Trump will host the first White House Crypto Summit on Friday March 7. Attendees will include prominent founders, CEOs, and investors from the crypto industry. Look forward to seeing everyone there!", 60, 90)
 }
 
