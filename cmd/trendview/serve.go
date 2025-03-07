f3dc80406184be698beddc0e51313a2291972357
Arnaud Aliès <arnaudalies.py@gmail.com>
2025-03-07 10:34:35 +0100

    Update serve.go

--- cmd/trendview/serve.go
+++ cmd/trendview/serve.go
@@ -9,7 +9,18 @@ import (
 	"net/http"
 
 	"github.com/mouuff/TrendView/pkg/itemstore"
+	"github.com/mouuff/TrendView/pkg/model"
 )
+
+// ItemComposite is a feed item with all the generated results
+type ItemBySubjectApiResponse struct {
+	model.FeedItem
+	Results map[string]int
+}
+
+type ItemsBySubjectApiResponse struct {
+	Items map[string]*ItemBySubjectApiResponse
+}
 
 // Ms describes the generate-trend subcommand
 // This command is used to generate trend
@@ -46,6 +57,39 @@ func (cmd *Serve) Run() error {
 	}
 	defer storage.Close()
 
+	http.HandleFunc("/itemsBySubject", func(w http.ResponseWriter, r *http.Request) {
+		// Only allow GET requests
+		if r.Method != http.MethodGet {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		subject := r.URL.Query().Get("subject")
+
+		if subject == "" {
+			http.Error(w, "Please specify the 'subject' parameter", http.StatusBadRequest)
+			return
+		}
+
+		// Fetch all items
+		items, err := storage.FindItems()
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Failed to fetch items: %v", err), http.StatusInternalServerError)
+			return
+		}
+
+		itemsBySubject := getItemsBySubject(items, subject)
+
+		// Set JSON content type
+		w.Header().Set("Content-Type", "application/json")
+
+		// Encode and send the response
+		if err := json.NewEncoder(w).Encode(itemsBySubject); err != nil {
+			http.Error(w, fmt.Sprintf("Failed to encode response: %v", err), http.StatusInternalServerError)
+			return
+		}
+	})
+
 	http.HandleFunc("/items", func(w http.ResponseWriter, r *http.Request) {
 		// Only allow GET requests
 		if r.Method != http.MethodGet {
@@ -77,4 +121,24 @@ func (cmd *Serve) Run() error {
 	}
 
 	return nil
+}
+
+func getItemsBySubject(itemState model.ItemCompositeMap, subject string) ItemsBySubjectApiResponse {
+	apiItems := make(map[string]*ItemBySubjectApiResponse)
+	for key, item := range itemState {
+		apiItem := &ItemBySubjectApiResponse{
+			FeedItem: item.FeedItem,
+		}
+
+		apiItem.Results = make(map[string]int)
+
+		for _, ratingResult := range item.Results {
+			if ratingResult.SubjectName == subject {
+				apiItem.Results[ratingResult.InsightName] = ratingResult.Value
+			}
+		}
+
+		apiItems[key] = apiItem
+	}
+	return ItemsBySubjectApiResponse{Items: apiItems}
 }
