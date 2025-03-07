43d2901e9e70e38c6f24e4c67662252c4f489d93
Arnaud Aliès <arnaudalies.py@gmail.com>
2025-03-07 09:35:23 +0100

    WIP serve

--- cmd/trendview/serve.go
+++ cmd/trendview/serve.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"context"
 	"errors"
 	"flag"
 	"log"
-	"time"
 
-	"github.com/mouuff/TrendView/internal"
-	"github.com/mouuff/TrendView/pkg/brain"
-	"github.com/mouuff/TrendView/pkg/generator"
 	"github.com/mouuff/TrendView/pkg/itemstore"
 )
 
@@ -18,10 +13,7 @@ import (
 type Serve struct {
 	flagSet *flag.FlagSet
 
-	config   string
 	datafile string
-	loop     bool
-	regen    bool
 }
 
 // Name gets the name of the command
@@ -32,7 +24,6 @@ func (cmd *Serve) Name() string {
 // Init initializes the command
 func (cmd *Serve) Init(args []string) error {
 	cmd.flagSet = flag.NewFlagSet(cmd.Name(), flag.ExitOnError)
-	cmd.flagSet.StringVar(&cmd.config, "config", "", "configuration file (required)")
 	cmd.flagSet.StringVar(&cmd.datafile, "datafile", "", "file used to load and store data (required)")
 	return cmd.flagSet.Parse(args)
 }
@@ -41,48 +32,16 @@ func (cmd *Serve) Init(args []string) error {
 func (cmd *Serve) Run() error {
 	log.Println("Starting server...")
 
-	if cmd.config == "" {
-		log.Println("Please pass the configuration file using -config")
-		log.Println("Here is an example configuration:")
-		printConfigurationTemplate()
-		return errors.New("-config parameter required")
-	}
 	if cmd.datafile == "" {
 		log.Println("Please specify a data file using -datafile (e.g. -datafile data.json)")
 		return errors.New("-datafile parameter required")
-	}
-
-	var config TrendGeneratorConfig
-	err := internal.ReadFromJson(cmd.config, &config)
-	if err != nil {
-		return err
-	}
-
-	brain, err := brain.NewOllamaBrain()
-	if err != nil {
-		return err
 	}
 
 	storage, err := itemstore.NewSQLiteItemStore(cmd.datafile)
 	if err != nil {
 		return err
 	}
+	defer storage.Close()
 
-	tg := &generator.TrendGenerator{
-		Context:       context.Background(),
-		Brain:         brain,
-		Storage:       storage,
-		Feeds:         internal.ConvertToFeedReaders(config.RssFeedReaders),
-		RatingPrompts: config.RatingPrompts,
-		ReGenerate:    cmd.regen,
-	}
-
-	if cmd.loop {
-		for {
-			tg.Execute()
-			time.Sleep(5 * time.Minute)
-		}
-	}
-
-	return tg.Execute()
+	return nil
 }
